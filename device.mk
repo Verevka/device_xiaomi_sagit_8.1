fdbcbe7dab95c8869802709b373784e6d3214e8f
verevka <verevka86@gmail.com>
2018-03-18 08:31:48 +0300

    sagit: switch to QTI HIDL binderized power 1.0 HAL

--- device.mk
+++ device.mk
@@ -305,9 +305,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service \
-    power.msm8998
+    android.hardware.power@1.0-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
