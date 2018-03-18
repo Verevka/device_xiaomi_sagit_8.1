5eb1ce5b440a7a80f21b3eef862353dc53fae9bf
verevka <verevka86@gmail.com>
2018-03-18 11:45:52 +0300

    sagit: add build contexthub hidl

--- device.mk
+++ device.mk
@@ -377,6 +377,8 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    context_hub.default \
+    android.hardware.contexthub@1.0-impl \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
